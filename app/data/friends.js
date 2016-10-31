38fefe8c820c82ed4e029fb20c7713782fd9a7dc
Michael Constanza <michael.constanza@gmail.com>
2016-10-30 20:56:50 -0400

    basic match algorithm functioning. returns friend object of best match.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,4 @@
+
 var friends = [
 	{
 		"name":"Mike",
@@ -13,6 +14,93 @@ var friends = [
 			2,
 			5,
 			5
+			]
+	
+	},
+	{
+		"name":"Nick",
+		"photo": '../public/nick.jpg',
+		"scores":[
+
+		3,
+		2,
+		3,
+		5,
+		4,
+		1,
+		2,
+		3,
+		4,
+		5
+		]
+	},
+	{
+		"name":"Aubrey",
+		"photo": '../public/nick.jpg',
+		"scores":[
+
+		5,
+		2,
+		3,
+		3,
+		2,
+		5,
+		2,
+		3,
+		4,
+		1
+		]
+
+	},
+	{
+		"name":"Bill",
+		"photo": '../public/bill.jpg',
+		"scores":[
+
+		5,
+		5,
+		3,
+		5,
+		2,
+		5,
+		5,
+		3,
+		4,
+		1
+		]
+	},
+	{
+		"name":"Christina",
+		"photo": '../public/christina.jpg',
+		"scores":[
+
+		1,
+		1,
+		3,
+		5,
+		2,
+		5,
+		5,
+		5,
+		5,
+		3
+		]
+	},
+	{
+		"name":"Scarlet",
+		"photo": '../public/scarjo.jpg',
+		"scores":[
+
+		1,
+		2,
+		3,
+		1,
+		2,
+		2,
+		5,
+		3,
+		2,
+		3
 		]
 	}
 ];
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -15,12 +15,72 @@ module.exports = function (app) {
 
 		friends.push(friend);
 
-		res.send(friend);
+		// res.send(friend); // test code
 
-		// compatiblity(friend);
+		console.log(friend)
+
+		var match = compatiblity(friend);
+
+		res.send('Here is your match: ' + JSON.stringify(match));
+
+		// loadMatch(match);
 	})
 
+
+
+// function to find the most compatible friend
 	function compatiblity(user) {
+
+
+		var lowestDiff = 50;
+		var friendLowestDiff = {};
+
+
+		for (i = 0; i < friends.length; i++) {
+
+			// compare scores with anyone that isn't the user
+			if (friends[i].name != user.name && friends[i].photo != user.photo) {
+
+				console.log(friends[i].name)
+				console.log(friends[i].scores)
+
+				// var diff = totalDifference(user.scores, friends[i].scores);
+				var diff = 0;
+
+				for (j = 0; j < user.scores.length; j++) {
+					diff += Math.abs(parseInt(user.scores[j]) - parseInt(friends[i].scores[j]));
+				}
+
+				console.log('diff: ' + diff)
+
+				// if the difference in scores is lower than that of all prev friends, they are a match
+				if (diff < lowestDiff) {
+
+					lowestDiff = diff;
+
+					console.log('friend ' + JSON.stringify(friends[i]))
+					friendLowestDiff = friends[i];
+
+					console.log(lowestDiff)
+
+					console.log('friend lowest diff:' + friendLowestDiff)
+				}
+			}
+		}
+		return friendLowestDiff
+	}
+
+
+// CURRENTLY NOT WORKING! ONLY WORKS IN BODY OF LOOP
+	function totalDifference(arr1, arr2) {
+
+		var totalDifference = 0;
+
+		for (i = 0; i < arr1.length; i++) {
+			totalDifference += Math.abs(parseInt(arr1[i]) - parseInt(arr2[i]));
+		};
+
+		return totalDifference
 
 	}
 }
